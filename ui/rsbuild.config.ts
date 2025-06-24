570c3bea341e484fe2e33b73a9bb16ec49836f87
Łukasz Nowakowski <github@lnow.pl>
2025-06-24 16:03:45 +0200

    Change page title.

--- ui/rsbuild.config.ts
+++ ui/rsbuild.config.ts
@@ -5,6 +5,6 @@ import { pluginSass } from '@rsbuild/plugin-sass';
 export default defineConfig({
   plugins: [pluginReact(), pluginSass()],
   html: {
-    title: 'Family Piggy Bank',
+    title: 'Project follow-up',
   },
 });

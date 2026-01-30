c1946095ec55ac6370910a149cc98967ffea68c4
Łukasz Nowakowski <lukasz@lnow.pl>
2026-01-30 23:13:18 +0100

    Force specific images versions and add MongoDB image.

--- docker/dev/rabbitmq/SetupRabbitMQ.ps1
+++ docker/dev/rabbitmq/SetupRabbitMQ.ps1
@@ -14,15 +14,15 @@ $commands = @(
     # QU.<service>.<consuming_entity>.<sendingentity>.<event> for queues
 
     # Create exchanges
-    'rabbitmqadmin -u admin -p admin --vhost "projectfollowup" declare exchange name="EX.bff.user.registered" type="fanout" durable=true auto_delete=false',
-    'rabbitmqadmin -u admin -p admin --vhost "projectfollowup" declare exchange name="EX.bff.activation-link.generated" type="fanout" durable=true auto_delete=false',
+    'rabbitmqadmin -u admin -p admin --vhost "projectfollowup" declare exchange --name="EX.bff.user.registered" --type="fanout" --durable=true --auto-delete=false',
+    'rabbitmqadmin -u admin -p admin --vhost "projectfollowup" declare exchange --name="EX.bff.activation-link.generated" --type="fanout" --durable=true --auto-delete=false',
 
     # Create queues
-    'rabbitmqadmin -u admin -p admin --vhost "projectfollowup" declare queue name="QU.bff.activation-link.user.registered" durable=true auto_delete=false',
-    'rabbitmqadmin -u admin -p admin --vhost "projectfollowup" declare queue name="QU.bff.documents.activation-link-generated" durable=true auto_delete=false',
+    'rabbitmqadmin -u admin -p admin --vhost "projectfollowup" declare queue --name="QU.bff.activation-link.user.registered" --durable=true --auto-delete=false',
+    'rabbitmqadmin -u admin -p admin --vhost "projectfollowup" declare queue --name="QU.bff.documents.activation-link-generated" --durable=true --auto-delete=false',
     # Bind queues to exchanges
-    'rabbitmqadmin -u admin -p admin --vhost projectfollowup declare binding source=EX.bff.user.registered destination_type=queue destination=QU.bff.activation-link.user.registered routing_key=""""',
-    'rabbitmqadmin -u admin -p admin --vhost projectfollowup declare binding source=EX.bff.activation-link.generated destination_type=queue destination=QU.bff.documents.activation-link-generated routing_key=""""'
+    'rabbitmqadmin -u admin -p admin --vhost projectfollowup declare binding --source=EX.bff.user.registered --destination-type=queue --destination=QU.bff.activation-link.user.registered --routing-key=""""',
+    'rabbitmqadmin -u admin -p admin --vhost projectfollowup declare binding --source=EX.bff.activation-link.generated --destination-type=queue --destination=QU.bff.documents.activation-link-generated --routing-key=""""'
 )
 
 foreach ($command in $commands) {

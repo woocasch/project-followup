813ac5805f91bd1e2ca81fdca18a995be897dbb4
Łukasz Nowakowski <lukasz@lnow.pl>
2026-01-31 22:56:11 +0100

    Extract base class for projection writers.

---
+++ bff/src/Infrastructure/ProjectionWriters/Mongo/ProjectionWriterBase.cs
@@ -0,0 +1,70 @@
+﻿namespace ProjectFollowUp.BFF.Infrastructure.ProjectionWriters.Mongo;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using MongoDB.Driver;
+
+using ProjectFollowUp.BFF.Application.EventSourcing;
+
+public abstract class ProjectionWriterBase<TRecord, TDto, TId> : IProjectionWriter<TRecord, TId>
+    where TRecord : struct
+{
+    public virtual async Task<TRecord?> Get(TId id, CancellationToken cancellationToken)
+    {
+        var collection = this.GetCollection();
+        var filterBuilder = Builders<TDto>.Filter;
+        var filter = this.ApplyFilterById(filterBuilder, id);
+        var searchOptions = new FindOptions<TDto>()
+        {
+            Limit = 1,
+        };
+        var itemsMatched = await collection.FindAsync(filter, searchOptions, cancellationToken)
+            .ConfigureAwait(false);
+        var items = await itemsMatched
+            .ToListAsync(cancellationToken)
+            .ConfigureAwait(false);
+        if (items.Count == 0)
+        {
+            return null;
+        }
+
+        var found = items[0];
+        return this.MapFromDto(found);
+    }
+
+    public virtual async Task Insert(TRecord record, CancellationToken cancellationToken)
+    {
+        var dto = this.MapFromRecord(record);
+        var collection = this.GetCollection();
+        await collection.InsertOneAsync(
+                dto,
+                null,
+                cancellationToken);
+    }
+
+    public virtual async Task Update(TRecord record, CancellationToken cancellationToken)
+    {
+        var filterBuilder = Builders<TDto>.Filter;
+        var filter = this.ApplyFilterById(filterBuilder, this.GetIdFromRecord(record));
+        var update = this.ApplyUpdateDefinition(Builders<TDto>.Update, record);
+        var collection = this.GetCollection();
+        await collection.UpdateOneAsync(
+                filter,
+                update,
+                null,
+                cancellationToken);
+    }
+
+    protected abstract IMongoCollection<TDto> GetCollection();
+
+    protected abstract FilterDefinition<TDto> ApplyFilterById(FilterDefinitionBuilder<TDto> filterBuilder, TId id);
+
+    protected abstract TRecord MapFromDto(TDto dto);
+
+    protected abstract TDto MapFromRecord(TRecord record);
+
+    protected abstract UpdateDefinition<TDto> ApplyUpdateDefinition(UpdateDefinitionBuilder<TDto> builder, TRecord record);
+
+    protected abstract TId GetIdFromRecord(TRecord record);
+}
--- bff/src/Infrastructure/ProjectionWriters/Mongo/UserProjectionWriter.cs
+++ bff/src/Infrastructure/ProjectionWriters/Mongo/UserProjectionWriter.cs
@@ -1,9 +1,5 @@
 ﻿namespace ProjectFollowUp.BFF.Infrastructure.ProjectionWriters.Mongo;
 
-using System.IdentityModel.Tokens.Jwt;
-using System.Threading;
-using System.Threading.Tasks;
-
 using MongoDB.Driver;
 
 using ProjectFollowUp.BFF.Application.Users.ProjectionWorkers;
@@ -12,72 +8,48 @@ using ProjectFollowUp.BFF.Domain.User;
 using ProjectFollowUp.BFF.Infrastructure.ProjectionWriters.Mongo.UserProjection;
 
 public sealed class UserProjectionWriter(
-    ICollectionProvider collectionProvider) : IUserProjectionWriter
+    ICollectionProvider collectionProvider) : ProjectionWriterBase<UserRecord, UserDto, UserId>, IUserProjectionWriter
 {
-    public async Task<UserRecord?> Get(UserId id, CancellationToken cancellationToken)
+    protected override FilterDefinition<UserDto> ApplyFilterById(FilterDefinitionBuilder<UserDto> filterBuilder, UserId id)
     {
-        var collection = this.GetCollection();
-        var filterBuilder = Builders<UserDto>.Filter;
-        var filter = filterBuilder.Eq(dto => dto.Id, id.Value);
-        var searchOptions = new FindOptions<UserDto>()
-        {
-            Limit = 1,
-        };
-        var itemsMatched = await collection.FindAsync(filter, searchOptions, cancellationToken)
-            .ConfigureAwait(false);
-        var items = await itemsMatched
-            .ToListAsync(cancellationToken)
-            .ConfigureAwait(false);
-        if (items.Count == 0)
-        {
-            return null;
-        }
-
-        var found = items[0];
-        return new(
-            UserId.FromGuid(found.Id),
-            found.CredentialsId,
-            EmailAddress.FromString(found.Email),
-            found.DisplayName,
-            found.CreatedAt);
+        return filterBuilder.Eq(dto => dto.Id, id.Value);
     }
 
-    public async Task Insert(UserRecord user, CancellationToken cancellationToken)
+    protected override UpdateDefinition<UserDto> ApplyUpdateDefinition(UpdateDefinitionBuilder<UserDto> builder, UserRecord record)
     {
-        var dto = new UserDto
-        {
-            Id = user.Id.Value,
-            CredentialsId = user.CredentialsId,
-            Email = user.Email.Value,
-            DisplayName = user.DisplayName,
-            CreatedAt = user.CreatedAt,
-        };
-        var collection = this.GetCollection();
-        await collection.InsertOneAsync(
-                dto,
-                null,
-                cancellationToken);
+        return builder
+            .Set(u => u.CredentialsId, record.CredentialsId)
+            .Set(u => u.Email, record.Email.ToString())
+            .Set(u => u.DisplayName, record.DisplayName)
+            .Set(u => u.CreatedAt, record.CreatedAt);
     }
 
-    public async Task Update(UserRecord user, CancellationToken cancellationToken)
-    {
-        var filterBuilder = Builders<UserDto>.Filter;
-        var filter = filterBuilder.Eq(dto => dto.Id, user.Id.Value);
-        var update = Builders<UserDto>.Update
-            .Set(u => u.CredentialsId, user.CredentialsId)
-            .Set(u => u.Email, user.Email.ToString())
-            .Set(u => u.DisplayName, user.DisplayName)
-            .Set(u => u.CreatedAt, user.CreatedAt);
-        var collection = this.GetCollection();
-        await collection.UpdateOneAsync(
-                filter,
-                update,
-                null,
-                cancellationToken);
-    }
-
-    private IMongoCollection<UserDto> GetCollection()
+    protected override IMongoCollection<UserDto> GetCollection()
     {
         return collectionProvider.Users;
     }
+
+    protected override UserRecord MapFromDto(UserDto dto)
+    {
+        return new(
+            UserId.FromGuid(dto.Id),
+            dto.CredentialsId,
+            EmailAddress.FromString(dto.Email),
+            dto.DisplayName,
+            dto.CreatedAt);
+    }
+
+    protected override UserDto MapFromRecord(UserRecord record)
+    {
+        return new UserDto
+        {
+            Id = record.Id.Value,
+            CredentialsId = record.CredentialsId,
+            Email = record.Email.Value,
+            DisplayName = record.DisplayName,
+            CreatedAt = record.CreatedAt,
+        };
+    }
+
+    protected override UserId GetIdFromRecord(UserRecord record) => record.Id;
 }

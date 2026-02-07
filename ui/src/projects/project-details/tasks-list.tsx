bed174b99d3ab1f63cd03e92d792cac5d89ae6ae
Łukasz Nowakowski <lukasz@lnow.pl>
2026-02-07 15:02:44 +0100

    Add some styling on the users' and projects' lists.
    Work on #20

--- ui/src/projects/project-details/tasks-list.tsx
+++ ui/src/projects/project-details/tasks-list.tsx
@@ -1,16 +1,25 @@
-import { LoadingSpinner, NamedPanel } from "@root/components";
+import { Button, LoadingSpinner, NamedPanel } from "@root/components";
 import type * as model from './project-details.model';
 import projectDetailsService from './project-details.service';
 import { useEffect, useState } from "react";
 import styled from "@emotion/styled";
+import { theme } from "@root/theme";
 
 export interface TasksListProps {
     projectId: string;
 }
 
 const ListStyled = styled.ul(`
+    margin: ${theme.spaces.medium};
     list-style: none;
     padding-left: 0;
+    &>li {
+        inline-width: 100%;
+        &>button {
+            width: 100%;
+            margin: ${theme.spaces.xsmall};
+        }
+    }
 `);
 
 export default function TasksList({ projectId }: TasksListProps) {
@@ -31,7 +40,9 @@ export default function TasksList({ projectId }: TasksListProps) {
 
     return (<NamedPanel title='Tasks'>
         {loadingTasks ? <LoadingSpinner /> : <ListStyled>
-            {tasks.map((task) => (<li key={task.id}>{task.title}</li>))}
+            {tasks.map((task) => (<li key={task.id}>
+                <Button buttonType="rounded" variant="success" title={task.title}>{task.title}</Button>
+            </li>))}
         </ListStyled>}
     </NamedPanel>);
 }
--- ui/src/projects/project-details/users-list.tsx
+++ ui/src/projects/project-details/users-list.tsx
@@ -1,16 +1,20 @@
-import { LoadingSpinner, NamedPanel } from "@root/components";
+import { Button, LoadingSpinner, NamedPanel } from "@root/components";
 import type * as model from './project-details.model';
 import projectDetailsService from './project-details.service';
 import { useEffect, useState } from "react";
 import styled from "@emotion/styled";
+import { theme } from "@root/theme";
 
 export interface UsersListProps {
     projectId: string;
 }
 
 const ListStyled = styled.ul(`
+    margin: ${theme.spaces.medium};
     list-style: none;
     padding-left: 0;
+    display: flex;
+    gap: ${theme.spaces.medium}
 `);
 
 export default function UsersList({ projectId }: UsersListProps) {
@@ -31,7 +35,10 @@ export default function UsersList({ projectId }: UsersListProps) {
 
     return (<NamedPanel title='Assigned users'>
         {loadingUsers ? <LoadingSpinner /> : <ListStyled>
-            {users.map((user) => (<li key={user.id}>{user.displayName}</li>))}
+            {users.map((user) => (
+                <li key={user.id}>
+                    <Button buttonType="rounded" variant="success" title={user.displayName}>{user.displayName}</Button>
+                </li>))}
         </ListStyled>}
     </NamedPanel>);
 }

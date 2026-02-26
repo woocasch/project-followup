945e9c8fb6b976932c8a97bdd7abc3096d5d00a0
Łukasz Nowakowski <lukasz@lnow.pl>
2026-02-27 00:58:06 +0100

    Add logging for projects controller.

--- bff/src/WebApi/Controllers/Projects/ProjectsController.cs
+++ bff/src/WebApi/Controllers/Projects/ProjectsController.cs
@@ -12,20 +12,25 @@ using ProjectFollowUp.BFF.WebApi.Controllers.Projects.ProjectsModels;
 
 [Route("api/[controller]")]
 [ApiController]
-public sealed class ProjectsController(IMediator mediator) : ControllerBase
+public sealed class ProjectsController(
+    IMediator mediator,
+    ILogger<ProjectsController> logger) : ControllerBase
 {
     public const string GetRouteName = "GetProjectById";
 
     [HttpGet]
     public async Task<IResult> FetchList(CancellationToken cancellationToken)
     {
+        logger.FetchListStarted();
         var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier) ?? User.FindFirst("sub");
         if (userIdClaim is null || !Guid.TryParse(userIdClaim.Value, out var userId))
         {
+            logger.FetchListUserNotAuthorized();
             return Results.Unauthorized();
         }
 
         var query = new FetchProjectsQuery(userId);
+        logger.FetchListFetchingData();
         var result = await mediator.Fetch(query, cancellationToken);
         var projects = result.Projects
             .Select(p => new ProjectListItem(
@@ -37,6 +42,7 @@ public sealed class ProjectsController(IMediator mediator) : ControllerBase
                 p.TasksTotal))
             .ToList();
         var output = new FetchListOutput([.. projects]);
+        logger.FetchListCompleted();
         return Results.Ok(output);
     }
 
@@ -45,16 +51,20 @@ public sealed class ProjectsController(IMediator mediator) : ControllerBase
         Guid projectId,
         CancellationToken cancellationToken)
     {
+        logger.GetStarted();
         var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier) ?? User.FindFirst("sub");
         if (userIdClaim is null || !Guid.TryParse(userIdClaim.Value, out var userId))
         {
+            logger.GetUserNotAuthorized();
             return Results.Unauthorized();
         }
 
         var query = new GetProjectQuery(userId, ProjectId.FromGuid(projectId));
+        logger.GetFetchingData();
         var result = await mediator.Fetch(query, cancellationToken);
         if (!result.ProjectExists)
         {
+            logger.GetProjectNotFound(projectId);
             return Results.NotFound();
         }
 
@@ -63,6 +73,7 @@ public sealed class ProjectsController(IMediator mediator) : ControllerBase
             project.ProjectId.ToGuid(),
             project.Title,
             project.Description);
+        logger.GetCompleted();
         return Results.Ok(output);
     }
 
@@ -71,13 +82,16 @@ public sealed class ProjectsController(IMediator mediator) : ControllerBase
         CreateInput payload,
         CancellationToken cancellationToken)
     {
+        logger.CreateStarted();
         var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier) ?? User.FindFirst("sub");
         if (userIdClaim is null || !Guid.TryParse(userIdClaim.Value, out var userId))
         {
+            logger.CreateUserNotAuthorized();
             return Results.Unauthorized();
         }
 
         var projectId = Guid.NewGuid();
+        logger.CreateCreatingProject();
         var command = new CreateProjectCommand(
             ProjectId.FromGuid(projectId),
             payload.Title,
@@ -87,9 +101,11 @@ public sealed class ProjectsController(IMediator mediator) : ControllerBase
         var result = await mediator.Send(command, cancellationToken);
         if (!result.IsSuccess)
         {
+            logger.CreateCreationFailed();
             return Results.Problem("Could not create project.");
         }
 
+        logger.CreateCompleted(projectId);
         return Results.CreatedAtRoute(GetRouteName, new { projectId }, new CreateOutput(projectId));
     }
 
@@ -99,12 +115,15 @@ public sealed class ProjectsController(IMediator mediator) : ControllerBase
         UpdateInput payload,
         CancellationToken cancellationToken)
     {
+        logger.UpdateStarted();
         var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier) ?? User.FindFirst("sub");
         if (userIdClaim is null || !Guid.TryParse(userIdClaim.Value, out var userId))
         {
+            logger.UpdateUserNotAuthorized();
             return Results.Unauthorized();
         }
 
+        logger.UpdateUpdatingProject(projectId);
         var command = new UpdateProjectCommand(
             ProjectId.FromGuid(projectId),
             payload.Title,
@@ -114,9 +133,11 @@ public sealed class ProjectsController(IMediator mediator) : ControllerBase
         var result = await mediator.Send(command, cancellationToken);
         if (!result.IsSuccess)
         {
+            logger.UpdateUpdateFailed(projectId);
             return Results.Problem("Could not update project.");
         }
 
+        logger.UpdateCompleted(projectId);
         return Results.AcceptedAtRoute(GetRouteName, new { projectId });
     }
 }
---
+++ bff/src/WebApi/Controllers/Projects/ProjectsControllerLogs.cs
@@ -0,0 +1,204 @@
+﻿namespace ProjectFollowUp.BFF.WebApi.Controllers.Projects;
+
+using Microsoft.AspNetCore.Server.Kestrel.Transport.NamedPipes;
+
+internal static partial class ProjectsControllerLogs
+{
+    [LoggerMessage(
+        EventId = EventIds.FetchStarted,
+        EventName = nameof(EventIds.FetchStarted),
+        Level = LogLevel.Trace,
+        Message = "Started fetching projects")]
+    public static partial void FetchListStarted(
+        this ILogger<ProjectsController> logger);
+
+    [LoggerMessage(
+        EventId = EventIds.FetchUserNotAuthorized,
+        EventName = nameof(EventIds.FetchUserNotAuthorized),
+        Level = LogLevel.Error,
+        Message = "User is not authorized to fetch projects")]
+    public static partial void FetchListUserNotAuthorized(
+        this ILogger<ProjectsController> logger);
+
+    [LoggerMessage(
+        EventId = EventIds.FetchFetchingData,
+        EventName = nameof(EventIds.FetchFetchingData),
+        Level = LogLevel.Trace,
+        Message = "Fetching projects data")]
+    public static partial void FetchListFetchingData(
+        this ILogger<ProjectsController> logger);
+
+    [LoggerMessage(
+        EventId = EventIds.FetchCompleted,
+        EventName = nameof(EventIds.FetchCompleted),
+        Level = LogLevel.Trace,
+        Message = "Completed fetching projects")]
+    public static partial void FetchListCompleted(
+        this ILogger<ProjectsController> logger);
+
+    [LoggerMessage(
+        EventId = EventIds.GetStarted,
+        EventName = nameof(EventIds.GetStarted),
+        Level = LogLevel.Trace,
+        Message = "Started fetching project details")]
+    public static partial void GetStarted(
+        this ILogger<ProjectsController> logger);
+
+    [LoggerMessage(
+        EventId = EventIds.GetUserNotAuthorized,
+        EventName = nameof(EventIds.GetUserNotAuthorized),
+        Level = LogLevel.Error,
+        Message = "User is not authorized to fetch project details")]
+    public static partial void GetUserNotAuthorized(
+        this ILogger<ProjectsController> logger);
+
+    [LoggerMessage(
+        EventId = EventIds.GetFetchingData,
+        EventName = nameof(EventIds.GetFetchingData),
+        Level = LogLevel.Trace,
+        Message = "Fetching project details data")]
+    public static partial void GetFetchingData(
+        this ILogger<ProjectsController> logger);
+
+    [LoggerMessage(
+        EventId = EventIds.GetProjectNotFound,
+        EventName = nameof(EventIds.GetProjectNotFound),
+        Level = LogLevel.Warning,
+        Message = "Project with id {ProjectId} was not found")]
+    public static partial void GetProjectNotFound(
+        this ILogger<ProjectsController> logger,
+        Guid projectId);
+
+    [LoggerMessage(
+        EventId = EventIds.GetCompleted,
+        EventName = nameof(EventIds.GetCompleted),
+        Level = LogLevel.Trace,
+        Message = "Completed fetching project details")]
+    public static partial void GetCompleted(
+        this ILogger<ProjectsController> logger);
+
+    [LoggerMessage(
+        EventId = EventIds.CreateStarted,
+        EventName = nameof(EventIds.CreateStarted),
+        Level = LogLevel.Trace,
+        Message = "Started creating project")]
+    public static partial void CreateStarted(
+        this ILogger<ProjectsController> logger);
+
+    [LoggerMessage(
+        EventId = EventIds.CreateUserNotAuthorized,
+        EventName = nameof(EventIds.CreateUserNotAuthorized),
+        Level = LogLevel.Error,
+        Message = "User is not authorized to create project")]
+    public static partial void CreateUserNotAuthorized(
+        this ILogger<ProjectsController> logger);
+
+    [LoggerMessage(
+        EventId = EventIds.CreateCreatingProject,
+        EventName = nameof(EventIds.CreateCreatingProject),
+        Level = LogLevel.Trace,
+        Message = "Creating project")]
+    public static partial void CreateCreatingProject(
+        this ILogger<ProjectsController> logger);
+
+    [LoggerMessage(
+        EventId = EventIds.CreateCreationFailed,
+        EventName = nameof(EventIds.CreateCreationFailed),
+        Level = LogLevel.Error,
+        Message = "Failed to create project")]
+    public static partial void CreateCreationFailed(
+        this ILogger<ProjectsController> logger);
+
+    [LoggerMessage(
+        EventId = EventIds.CreateCompleted,
+        EventName = nameof(EventIds.CreateCompleted),
+        Level = LogLevel.Trace,
+        Message = "Completed creating project with id {ProjectId}")]
+    public static partial void CreateCompleted(
+        this ILogger<ProjectsController> logger,
+        Guid projectId);
+
+    [LoggerMessage(
+        EventId = EventIds.UpdateStarted,
+        EventName = nameof(EventIds.UpdateStarted),
+        Level = LogLevel.Trace,
+        Message = "Started updating project")]
+    public static partial void UpdateStarted(
+        this ILogger<ProjectsController> logger);
+
+    [LoggerMessage(
+        EventId = EventIds.UpdateUserNotAuthorized,
+        EventName = nameof(EventIds.UpdateUserNotAuthorized),
+        Level = LogLevel.Error,
+        Message = "User is not authorized to update project")]
+    public static partial void UpdateUserNotAuthorized(
+        this ILogger<ProjectsController> logger);
+
+    [LoggerMessage(
+        EventId = EventIds.UpdateUpdatingProject,
+        EventName = nameof(EventIds.UpdateUpdatingProject),
+        Level = LogLevel.Trace,
+        Message = "Updating project with id {ProjectId}")]
+    public static partial void UpdateUpdatingProject(
+        this ILogger<ProjectsController> logger,
+        Guid projectId);
+
+    [LoggerMessage(
+        EventId = EventIds.UpdateUpdateFailed,
+        EventName = nameof(EventIds.UpdateUpdateFailed),
+        Level = LogLevel.Error,
+        Message = "Failed to update project with id {ProjectId}")]
+    public static partial void UpdateUpdateFailed(
+        this ILogger<ProjectsController> logger,
+        Guid projectId);
+
+    [LoggerMessage(
+        EventId = EventIds.UpdateCompleted,
+        EventName = nameof(EventIds.UpdateCompleted),
+        Level = LogLevel.Trace,
+        Message = "Completed updating project with id {ProjectId}")]
+    public static partial void UpdateCompleted(
+        this ILogger<ProjectsController> logger,
+        Guid projectId);
+
+    private static class EventIds
+    {
+        public const int FetchStarted = 1;
+
+        public const int FetchUserNotAuthorized = 2;
+
+        public const int FetchFetchingData = 3;
+
+        public const int FetchCompleted = 4;
+
+        public const int GetStarted = 5;
+
+        public const int GetUserNotAuthorized = 6;
+
+        public const int GetFetchingData = 7;
+
+        public const int GetProjectNotFound = 8;
+
+        public const int GetCompleted = 9;
+
+        public const int CreateStarted = 10;
+
+        public const int CreateUserNotAuthorized = 11;
+
+        public const int CreateCreatingProject = 12;
+
+        public const int CreateCreationFailed = 13;
+
+        public const int CreateCompleted = 14;
+
+        public const int UpdateStarted = 15;
+
+        public const int UpdateUserNotAuthorized = 16;
+
+        public const int UpdateUpdatingProject = 17;
+
+        public const int UpdateUpdateFailed = 18;
+
+        public const int UpdateCompleted = 19;
+    }
+}
